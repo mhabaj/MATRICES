6a7b0f76f1be04682b7523ad2c49a74485785d9e
ALHABAJ Mahmod <mhabaj99@hotmail.com>
2020-03-31 13:31:24 +0200

    ...

--- MATRICES/MATRICES/CMatrice.h
+++ MATRICES/MATRICES/CMatrice.h
@@ -1,12 +1,31 @@
 #pragma once
+#include <iostream> 
+#include <string>   
+using namespace std;
 
 template <typename Type>
 
 class CMatrice
 {
+
 private:
+	int inbCol;
+	int inbLig;
+	Type** ptrptrMatrice;
+
+
+public:
+
 	CMatrice();
+	CMatrice(string sPath);
 	CMatrice(int iLigne, int iColonne);
 	CMatrice(int iLigne, int iColonne, Type* pTypeTableauElems);
+	CMatrice(Type** paramMat);
+	void MatModifierElem(Type elem, int ligne, int colonne);
+	void MatAjoutLigne();
+	void MatAjoutColonne();
+
+
+
 
 };
