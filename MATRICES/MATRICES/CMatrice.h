94d05e56c012f1d2c4b3dfe0bc907e5ac0309bca
unknown <beldatom37@gmail.com>
2020-03-31 12:52:33 +0200

    uml use case/classes/start code

---
+++ MATRICES/MATRICES/CMatrice.h
@@ -0,0 +1,12 @@
+#pragma once
+
+template <typename Type>
+
+class CMatrice
+{
+private:
+	CMatrice();
+	CMatrice(int iLigne, int iColonne);
+	CMatrice(int iLigne, int iColonne, Type* pTypeTableauElems);
+
+};
